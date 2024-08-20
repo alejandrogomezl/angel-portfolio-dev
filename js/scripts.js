1943cb22769f350b4e123473d13da78c51fcd232
Alejandro Gomez Lozano <alejandrogomezlozano@hotmail.com>
2024-08-20 17:57:53 +0200

    Falta inicio sesión

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,49 @@
 function openPage(url) {
     window.location.href = url;
 }
+
+// script.js
+
+let screensaver;
+let screensaverImage;
+let xPos = 100, yPos = 100;
+let xSpeed = 2, ySpeed = 2;
+let maxX, maxY;
+
+function moveImage() {
+    xPos += xSpeed;
+    yPos += ySpeed;
+
+    if (xPos + screensaverImage.width >= maxX || xPos <= 0) {
+        xSpeed = -xSpeed;
+    }
+    if (yPos + screensaverImage.height >= maxY || yPos <= 0) {
+        ySpeed = -ySpeed;
+    }
+
+    screensaverImage.style.left = xPos + 'px';
+    screensaverImage.style.top = yPos + 'px';
+}
+
+function showScreensaver() {
+    screensaver.style.display = 'flex';
+    maxX = window.innerWidth;
+    maxY = window.innerHeight;
+    setInterval(moveImage, 10); // Velocidad de movimiento
+}
+
+function resetTimer() {
+    screensaver.style.display = 'none';
+    clearTimeout(screensaver.timer);
+    screensaver.timer = setTimeout(showScreensaver, 60000); // 30 segundos de inactividad
+}
+
+window.onload = () => {
+    screensaver = document.getElementById('screensaver');
+    screensaverImage = screensaver.querySelector('img');
+    window.onmousemove = resetTimer;
+    window.onkeypress = resetTimer;
+    window.onscroll = resetTimer;
+    window.onclick = resetTimer;
+    resetTimer(); // Iniciar el temporizador
+}
